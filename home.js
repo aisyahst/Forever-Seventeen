34af215b1548c838a39cd8055d25685389071bc9
Aisyah <saisyahhicah16@gmail.com>
2024-06-14 14:00:18 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,68 @@
+// OPENING A PROFILE TAB 
+    document.getElementById('person1').addEventListener('click', 
+    function() {
+        document.querySelector('.modal1').style.display = 'flex';
+    });
+
+    document.getElementById('person2').addEventListener('click', 
+    function() {
+        document.querySelector('.modal2').style.display = 'flex';
+    });
+
+    document.getElementById('person3').addEventListener('click', 
+    function() {
+        document.querySelector('.modal3').style.display = 'flex';
+    });
+
+    document.getElementById('person4').addEventListener('click', 
+    function() {
+        document.querySelector('.modal4').style.display = 'flex';
+    });
+
+    document.getElementById('person5').addEventListener('click', 
+    function() {
+        document.querySelector('.modal5').style.display = 'flex';
+    });
+
+    document.getElementById('person6').addEventListener('click', 
+    function() {
+        document.querySelector('.modal6').style.display = 'flex';
+    });
+
+    document.getElementById('person7').addEventListener('click', 
+    function() {
+        document.querySelector('.modal7').style.display = 'flex';
+    });
+
+
+
+
+    /// CLOSING A MODAL 
+    document.querySelector('.close-me1').addEventListener('click',
+    function() {
+        document.querySelector('.modal1').style.display = 'none';
+    });
+
+    document.querySelector('.close-me2').addEventListener('click', function() {
+            document.querySelector('.modal2').style.display = 'none';
+        });
+    
+    document.querySelector('.close-me3').addEventListener('click', function() {
+            document.querySelector('.modal3').style.display = 'none';
+        });
+
+    document.querySelector('.close-me4').addEventListener('click', function() {
+            document.querySelector('.modal4').style.display = 'none';
+        });
+
+     document.querySelector('.close-me5').addEventListener('click', function() {
+            document.querySelector('.modal5').style.display = 'none';
+        });
+  
+    document.querySelector('.close-me6').addEventListener('click', function() {
+            document.querySelector('.modal6').style.display = 'none';
+        });
+
+     document.querySelector('.close-me7').addEventListener('click', function() {
+            document.querySelector('.modal7').style.display = 'none';
+        });
